e2a98ea4661affafde086557746c028c76030830
toyopppe <hvggit.bv@gmail.com>
2017-02-08 00:19:51 +0900

    porker

--- porker.py
+++ porker.py
@@ -21,6 +21,21 @@ class Deck:
 				self.deck.append(lis)
 		return self.deck
 
+	def prize_dic(self):
+		dic = {
+			0:"No Pair",
+			1:"One Pair",
+			2:"Two Pair",
+			3:"Three of a Kind",
+			4:"Straight",
+			5:"Flush",
+			6:"Full House",
+			7:"Four of a Kind",
+			9:"Straight Flush",
+			10:"Royal Straight Flush"
+		}
+		return dic
+
 class Player:
 	def __init__(self):
 		self.hand = list
@@ -34,8 +49,8 @@ class Player:
 			deck_num -= 1
 		return return_cards
 
-	def change(self, name, deck, deck_num):
-		print "\n%s, Prease enter the number you want to change"%name
+	def change(self, deck, deck_num):
+		print "\n%s, Prease enter the number you want to change"%self.name
 		change = raw_input()
 		change = map(int, change)
 		for r in change:
@@ -44,56 +59,41 @@ class Player:
 				deck_num -= 1
 		return len(change)
 
-def print_hand(name, hand):
-	print " \t -*- %s's hand -*- \t "%name
-	print " 1\t 2\t 3\t 4\t 5\t"
-	for i in hand:
-		h = map(str, i)
-		print "-".join(h) + "\t",
-	print ""
+	def print_hand(self):
+		print " \t -*- %s's hand -*- \t "%self.name
+		print " 1\t 2\t 3\t 4\t 5\t"
+		for i in self.hand:
+			h = map(str, i)
+			print "-".join(h) + "\t",
+		print ""
 
-def dis(name, hand):
-	flush = 0
-	straight = 0
-	pair = 0
-	prize = 0
-	hand = sorted(hand, key=lambda x:x[1])
-	suto = hand[0][0]
-	for i, h in enumerate(hand):
-		#フラッシュ判別
-		if h[0] == suto: flush += 1
-		#ストレート判別
-		if i == 0: num = h[1]
-		else:
-			if num+1 == h[1]:
-				straight += 1
-				num = h[1]
-	#ペア判別
-	for i in range(4):
-		for j in range(i+1, 5):
-			if hand[i][1] == hand[j][1]:
-				pair += 1
-	if pair == 4: prize == 6
-	elif pair == 6: prize == 7
-	else: prize = pair
-	if straight == 4: prize += 4
-	if flush == 5: prize += 5
-	return prize
-
-def prize_dic():
-	dic = {
-		0:"No Pair",
-		1:"One Pair",
-		2:"Two Pair",
-		3:"Three of a Kind",
-		4:"Straight",
-		5:"Flush",
-		6:"Full House",
-		7:"Four of a Kind",
-		9:"Straight Flush",
-		10:"Royal Straight Flush"
-	}
-	return dic
+	def judge(self):
+		flush = 0
+		straight = 0
+		pair = 0
+		prize = 0
+		hand = sorted(self.hand, key=lambda x:x[1])
+		suto = self.hand[0][0]
+		for i, h in enumerate(self.hand):
+			#フラッシュ判別
+			if h[0] == suto: flush += 1
+			#ストレート判別
+			if i == 0: num = h[1]
+			else:
+				if num+1 == h[1]:
+					straight += 1
+					num = h[1]
+		#ペア判別
+		for i in range(4):
+			for j in range(i+1, 5):
+				if self.hand[i][1] == self.hand[j][1]:
+					pair += 1
+		if pair == 4: prize == 6
+		elif pair == 6: prize == 7
+		else: prize = pair
+		if straight == 4: prize += 4
+		if flush == 5: prize += 5
+		return prize
 
 def main():
 	d = Deck()
@@ -107,17 +107,18 @@ def main():
 	d.deck_num -= 5
 	p2.hand = sorted(p2.deal_card(d.deck, d.deck_num, 5))
 	d.deck_num -= 5
-	print_hand(p1.name, p1.hand)
-	print_hand(p2.name, p2.hand)
+	p1.print_hand()
+	p2.print_hand()
 	#手札を変える
-	d.deck_num -= p1.change(p1.name, d.deck, d.deck_num)
-	d.deck_num -= p2.change(p2.name, d.deck, d.deck_num)
-	dic = prize_dic()
-	print_hand(p1.name, p1.hand)
-	result1 = dis(p1.name, p1.hand)
+	d.deck_num -= p1.change(d.deck, d.deck_num)
+	d.deck_num -= p2.change(d.deck, d.deck_num)
+	#役判別
+	dic = d.prize_dic()
+	p1.print_hand()
+	result1 = p1.judge()
 	print dic[result1]
-	print_hand(p2.name, p2.hand)
-	result2 = dis(p2.name, p2.hand)
+	p2.print_hand()
+	result2 = p2.judge()
 	print dic[result2]
 	if result1 > result2: print "\n --%s is win!"%p1.name
 	elif result1 < result2: print "\n --%s is win!"%p2.name
